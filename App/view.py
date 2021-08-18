7401e4ffa84e0ead8865788b8c4be710638e9bbf
Daniela Camacho <d.camachom@uniandes.edu.co>
2021-08-17 19:30:54 -0500

    se agrego elif para nueva opción

--- App/view.py
+++ App/view.py
@@ -78,6 +78,12 @@ while True:
         print('Total de tags cargados: ' + str(lt.size(tags)))
 
     # TODO: Modificaciones para el laboratorio 1.
+    elif int(inputs[0]) == 3:
+        print("Cargando información de books y tags....")
+        tags = loadTags()
+        books = loadBooks()
+        print('Total de tags cargados: ' + str(lt.size(tags)))
+        print('Total de books cargados: ' + str(lt.size(books)))
 
     else:
         sys.exit(0)
